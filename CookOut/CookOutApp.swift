282eb5dec5f29a00752c87f35487b27839d77e9e
BumbleBee <bumblebeeshark@icloud.com>
2026-08-29 11:53:04 -0700

    Initial Commit

---
+++ CookOut/CookOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookOutApp.swift
+//  CookOut
+//
+//  Created by Bumblebee on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct CookOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
